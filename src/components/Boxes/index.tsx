af41635f33f842f7296079700a960848089b64df
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-06-12 00:42:47 -0300

    add grid system to boxes

--- src/components/Boxes/index.tsx
+++ src/components/Boxes/index.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
-import BoxColor from './BoxColor/BoxColor'
+import React from "react";
+import BoxColor from "./BoxColor/BoxColor";
+
+import styled from "styled-components";
 
 interface Props {
   colors: string[];
 }
 
+const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(13rem, 1fr));
+`;
+
 const Boxes: React.FC<Props> = (props) => {
   return (
-    <div>
-      { props.colors.map(color => (
+    <Container>
+      {props.colors.map((color) => (
         <BoxColor key={color} color={color} />
-      )) }
-    </div>
-  )
-}
+      ))}
+    </Container>
+  );
+};
 
-export default Boxes
+export default Boxes;
