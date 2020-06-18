1a1e65f4c3ca327f7a5999d60be0f4013c3f49c5
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-06-18 17:52:46 -0300

    some adjusts

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -1,35 +1,37 @@
-import React, { useState, useEffect } from "react";
-import Boxes from "./components/Boxes";
-import ColorContext from "./context/ColorContext";
+import React, { useState, useEffect } from 'react';
+import Boxes from './components/Boxes';
+import ColorContext from './context/ColorContext';
 import * as crypto from 'crypto';
 
 const ColorPicker = () => {
-  const [currentColor, setCurrentColor] = useState<string>("");
-  const [randomColors, setRandomColors] = useState<string[]>([])
-  
+  const [currentColor, setCurrentColor] = useState<string>('');
+  const [randomColors, setRandomColors] = useState<string[]>([]);
+
   const handlePickedColor = (color: string) => {
     setCurrentColor(color);
   };
 
   useEffect(() => {
-    const colors = []
+    const colors = [];
     for (let c = 0; c < 100; c++) {
-      colors.push(`#${crypto.randomBytes(3).toString('hex')}`)
+      colors.push(`#${crypto.randomBytes(3).toString('hex')}`);
     }
 
-    setRandomColors(colors)
-  }, [])
+    setRandomColors(colors);
+  }, []);
 
   return (
-    <div style={{width: '85vw', margin: 'auto auto'}}>
-      <ColorContext.Provider
-        value={{
-          pickedColor: handlePickedColor,
-        }}
-      >
-        <Boxes colors={randomColors} />
-      </ColorContext.Provider>
-    </div>
+    <>
+      <div style={{ width: '85vw', margin: 'auto' }}>
+        <ColorContext.Provider
+          value={{
+            pickedColor: handlePickedColor,
+          }}
+        >
+          <Boxes colors={randomColors} />
+        </ColorContext.Provider>
+      </div>
+    </>
   );
 };
 
