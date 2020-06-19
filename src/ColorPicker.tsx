605bb3eba645a34304738646e20816bec2584a6c
Lucas Rocha <lucasgeorgerocha@gmail.com>
2020-06-18 23:50:55 -0300

    show dynamically notifications on picking color

--- src/ColorPicker.tsx
+++ src/ColorPicker.tsx
@@ -1,21 +1,24 @@
-import React, { useState, useEffect } from 'react';
-import Boxes from './components/Boxes';
-import ColorContext from './context/ColorContext';
-import * as crypto from 'crypto';
-import Notification from './components/Notification'
+import React, { useState, useEffect } from "react";
+import Boxes from "./components/Boxes";
+import ColorContext from "./context/ColorContext";
+import * as crypto from "crypto";
+import Notification from "./components/Notification";
 
 const ColorPicker = () => {
-  const [currentColor, setCurrentColor] = useState<string>('');
+  const [currentColor, setCurrentColor] = useState<string>("");
   const [randomColors, setRandomColors] = useState<string[]>([]);
+  const [pickedColors, setPickedColors] = useState<string[]>([]);
 
   const handlePickedColor = (color: string) => {
+    setPickedColors([...pickedColors, color]);
+
     setCurrentColor(color);
   };
 
   useEffect(() => {
     const colors = [];
     for (let c = 0; c < 100; c++) {
-      colors.push(`#${crypto.randomBytes(3).toString('hex')}`);
+      colors.push(`#${crypto.randomBytes(3).toString("hex")}`);
     }
 
     setRandomColors(colors);
@@ -23,12 +26,24 @@ const ColorPicker = () => {
 
   return (
     <>
-    <div style={{position: 'fixed', width: '300px', top: '10px', right: '15px'}}>
-      <Notification title='Success' type='success' description='The copy get success' />
-      <Notification title='Success' type='success' description='The copy get success' />
-      <Notification title='Success' type='success' description='The copy get success' />
-    </div>
-      <div style={{ width: '85vw', margin: 'auto' }}>
+      <div
+        style={{
+          position: "fixed",
+          width: "300px",
+          top: "10px",
+          right: "15px",
+        }}
+      >
+        {pickedColors.map((pickedColor, index) => (
+          <Notification
+            key={`${pickedColor}_${index}`}
+            title="Success"
+            type="success"
+            description={`The copy get success with color ${pickedColor}`}
+          />
+        ))}
+      </div>
+      <div style={{ width: "85vw", margin: "auto" }}>
         <ColorContext.Provider
           value={{
             pickedColor: handlePickedColor,
